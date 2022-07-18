515a2f01f750af307e50852dd1785fd47217cd0a
ryosuke1202 <ryosuke12021@gmail.com>
2022-07-19 00:32:43 +0900

    todo編集、削除

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"log"
 	"net/http"
+
+	"todo/app/models"
 )
 
 // この中の引数はお決まりの書き方。こうすればハンドラーとして登録できる
@@ -29,4 +31,95 @@ func index(w http.ResponseWriter, r *http.Request) {
 		generateHTML(w, user, "layout", "private_navbar", "index")
 	}
 
+}
+
+func todoNew(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "todo_new")
+	}
+
+}
+
+func todoSave(w http.ResponseWriter, r *http.Request) {
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := session.GetUserBySession()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		content := r.PostFormValue("content")
+		if err := user.CreateTodo(content); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoEdit(w http.ResponseWriter, r *http.Request, id int) {
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := session.GetUserBySession()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todo, err := models.GetTodo(id)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		generateHTML(w, todo, "layout", "private_navbar", "todo_edit")
+	}
+}
+
+func todoUpdate(w http.ResponseWriter, r *http.Request, id int) {
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user, err := session.GetUserBySession()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		content := r.PostFormValue("content")
+		todo := &models.Todo{ID: id, Content: content, UserId: user.ID}
+		if err := todo.UpdateTodo(); err != nil {
+			log.Println(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func todoDelete(w http.ResponseWriter, r *http.Request, id int) {
+	session, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/login", 302)
+	} else {
+		_, err := session.GetUserBySession()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		todo, err := models.GetTodo(id)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		if err := todo.DeleteTodo(); err != nil {
+			log.Fatalln(err)
+		}
+		http.Redirect(w, r, "/todos", 302)
+	}
 }
--- app/controllers/server.go
+++ app/controllers/server.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"regexp"
+	"strconv"
 	"text/template"
 	"todo/app/models"
 
@@ -30,6 +32,24 @@ func session(w http.ResponseWriter, r *http.Request) (models.Session, error) {
 	return session, err
 }
 
+var validPath = regexp.MustCompile("^/todos/(edit|update|delete)/([0-9]+)$")
+
+func parseURL(fn func(http.ResponseWriter, *http.Request, int)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		q := validPath.FindStringSubmatch(r.URL.Path) // マッチした部分をスライスで取得
+		if q == nil {
+			http.NotFound(w, r)
+			return
+		}
+		q1, err := strconv.Atoi(q[2]) // URL末尾のidが入っている
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, q1)
+	}
+}
+
 func StartMainServer() error {
 	files := http.FileServer(http.Dir(config.Config.Static))
 	http.Handle("/static/", http.StripPrefix("/static/", files))
@@ -40,6 +60,11 @@ func StartMainServer() error {
 	http.HandleFunc("/authenticate", authenticate)
 	http.HandleFunc("/todos", index)
 	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/todos/new", todoNew)
+	http.HandleFunc("/todos/save", todoSave)
+	http.HandleFunc("/todos/edit/", parseURL(todoEdit))
+	http.HandleFunc("/todos/update/", parseURL(todoUpdate))
+	http.HandleFunc("/todos/delete/", parseURL(todoDelete))
 
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
